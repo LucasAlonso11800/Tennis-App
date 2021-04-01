b3609e7738819d2be1665dc6044dc23c8771a626
Lucas Alonso <lucasalonso11800@gmail.com>
2021-03-31 23:03:54 -0300

    Finally fixed navbar and icon

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,48 +5,26 @@ function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div id="atp-logo" className="mr-4"></div>
-            <Link to="/" className="navbar-brand">Tennis world</Link>
-            <div className="collapse navbar-collapse">
-                <div className="d-flex justify-content-around">
-                    <div className="dropdown">
-                        <button className="btn text-white dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Rankings
-                        </button>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <Link to="/ranking" className="dropdown-item">Ranking ATP / WTA</Link>
-                            <div className="dropdown-divider"></div>
-                            <Link to="/race-to-london" className="dropdown-item">Race to London</Link>
-                        </div>
-                    </div>
-                    <div className="dropdown">
-                        <button className="btn text-white dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Tournaments
-                        </button>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <Link to="/current-tournaments" className="dropdown-item">Current Tournaments</Link>
-                            <div className="dropdown-divider"></div>
-                            <Link to="/season" className="dropdown-item">Season Calendar</Link>
-                        </div>
-                    </div>
-                    <div className="nav-item">
-                        <Link to="/news" className="nav-link text-white">News</Link>
-                    </div>
+            <Link to="/" className="navbar-brand mr-auto">Tennis world</Link>
+            <div className="ml-4 d-flex">
+                <div className="mt-2 mr-4">
+                    <Link to="/ranking" className="nav-item text-center d-block text-white mb-2">Ranking ATP / WTA</Link>
+                    <Link to="/race-to-london" className="nav-item text-center d-block text-white mb-2">Race to London</Link>
                 </div>
-                <div className="ml-auto mr-4">
-                    <div className="dropdown">
-                        <button className="btn text-white dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Profile
-                        </button>
-                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <Link to="/user-articles" className="dropdown-item">Your saved Articles</Link>
-                            <div className="dropdown-divider"></div>
-                            <Link to="/signin" className="dropdown-item">Log in</Link>
-                            <Link to="/signup" className="dropdown-item">Sign up</Link>
-                        </div>
-                    </div>
+                <div className="mt-2 mr-4">
+                    <Link to="/current-tournaments" className="nav-item text-center d-block text-white mb-2">Current Tournaments</Link>
+                    <Link to="/season" className="nav-item text-center d-block text-white mb-2">Season Calendar</Link>
+                </div>
+                <div className="mt-2 mr-4">
+                    <Link to="/news" className="nav-item text-center d-block text-white mb-2">News</Link>
+                    <Link to="/user-articles" className="nav-item text-center d-block text-white mb-2">Your saved Articles</Link>
                 </div>
             </div>
-            <div className="ml-auto" id="wta-logo"></div>
+            <div className="mt-2 ml-auto">
+                <Link to="/signin" className="nav-item text-center d-block text-white mb-2">Log in</Link>
+                <Link to="/signup" className="nav-item text-center d-block text-white mb-2">Sign up</Link>
+            </div>
+            <div className="ml-4" id="wta-logo"></div>
         </nav>
     )
 }
