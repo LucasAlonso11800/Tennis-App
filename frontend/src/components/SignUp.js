555ca06e3ad42597d3e9e31ba103b56d6e6d428a
Lucas Alonso <lucasalonso11800@gmail.com>
2021-04-06 17:27:43 -0300

    Changed sign up redirection

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -5,7 +5,8 @@ function SignUp() {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [authError, setAuthError] = useState()
+    const [authSuccess, setAuthSuccess] = useState('')
+    const [authError, setAuthError] = useState('');
 
     function saveUser(e) {
         e.preventDefault()
@@ -15,13 +16,15 @@ function SignUp() {
             password: password
         })
             .then(res => {
+                setAuthSuccess("You've succesfully registered")
+                setAuthError('')
                 setUsername('');
                 setEmail('');
                 setPassword('');
-                window.location = '/signin';
             })
             .catch(err => {
                 if (err) {
+                    setAuthSuccess('')
                     setAuthError('Email already registered')
                 }
             })
@@ -33,49 +36,60 @@ function SignUp() {
                 <div className="col-sm-8 col-md-4">
                     <div className="card mt-4">
                         <div className="card-body">
-                            <h4>Create account</h4>
-                            <p>Sign up and save your favourite articles about your favourite players and tournaments</p>
-                            {authError ?
-                                <div className="alert alert-warning">{authError}</div>
-                                : <></>
+                            {authSuccess ?
+                                <>
+                                    <h4 className="text-center">{authSuccess}</h4>
+                                    <div className="d-flex justify-content-center">
+                                        <Link to="/signin" className="btn btn-primary my-2">Sign in</Link>
+                                    </div>
+                                </>
+                                :
+                                <>
+                                    <h4>Create account</h4>
+                                    <p>Sign up and save your favourite articles about your favourite players and tournaments</p>
+                                    {authError ?
+                                        <div className="alert alert-warning">{authError}</div>
+                                        :
+                                        <form onSubmit={saveUser}>
+                                            <div className="form-group">
+                                                <label>Username</label>
+                                                <input
+                                                    type="text"
+                                                    className="form-control"
+                                                    required
+                                                    value={username}
+                                                    onChange={e => setUsername(e.target.value)}
+                                                    placeholder='Enter your Username'
+                                                ></input>
+                                            </div>
+                                            <div className="form-group">
+                                                <label>Email</label>
+                                                <input
+                                                    type="email"
+                                                    className="form-control"
+                                                    required
+                                                    value={email}
+                                                    onChange={e => setEmail(e.target.value)}
+                                                    placeholder='Enter your Email'
+                                                ></input>
+                                            </div>
+                                            <div className="form-group">
+                                                <label>Password</label>
+                                                <input
+                                                    type="password"
+                                                    className="form-control"
+                                                    required
+                                                    value={password}
+                                                    onChange={e => setPassword(e.target.value)}
+                                                    placeholder='Enter your Password'
+                                                    minLength='6'
+                                                ></input>
+                                            </div>
+                                            <button type="submit" className="btn btn-primary w-100">Create account</button>
+                                        </form>
+                                    }
+                                </>
                             }
-                            <form onSubmit={saveUser}>
-                                <div className="form-group">
-                                    <label>Username</label>
-                                    <input
-                                        type="text"
-                                        className="form-control"
-                                        required
-                                        value={username}
-                                        onChange={e => setUsername(e.target.value)}
-                                        placeholder='Enter your Username'
-                                    ></input>
-                                </div>
-                                <div className="form-group">
-                                    <label>Email</label>
-                                    <input
-                                        type="email"
-                                        className="form-control"
-                                        required
-                                        value={email}
-                                        onChange={e => setEmail(e.target.value)}
-                                        placeholder='Enter your Email'
-                                    ></input>
-                                </div>
-                                <div className="form-group">
-                                    <label>Password</label>
-                                    <input
-                                        type="password"
-                                        className="form-control"
-                                        required
-                                        value={password}
-                                        onChange={e => setPassword(e.target.value)}
-                                        placeholder='Enter your Password'
-                                        minLength='6'
-                                    ></input>
-                                </div>
-                                <button type="submit" className="btn btn-primary w-100">Create account</button>
-                            </form>
                         </div>
                     </div>
                 </div>
