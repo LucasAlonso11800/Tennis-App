8209bc0d95e4de8ff7d3772066057b9af185afc9
Lucas Alonso <lucasalonso11800@gmail.com>
2021-04-05 23:56:20 -0300

    Addes two remaining miniatures

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -5,19 +5,27 @@ function LandingPage() {
     return (
         <div className="landing-page-container container-fluid">
             <div className="row d-flex justify-content-around">
-                <div className="mt-4 mb-3 col-sm-10 col-md-5 col-lg-4 d-flex justify-content-center align-items-center" id="australia">
+                <div className="mt-4 mb-3 col-sm-10 col-md-5 col-lg-4 d-flex justify-content-center align-items-center miniature" id="australia">
                     <Link className="text-white text-center h5" to="/ranking">Ranking ATP / WTA</Link>
                 </div>
-                <div className="mt-4 mb-3 col-sm-10 col-md-5 col-lg-4 d-flex justify-content-center align-items-center" id="roland-garros">
+                <div className="mt-4 mb-3 col-sm-10 col-md-5 col-lg-4 d-flex justify-content-center align-items-center miniature" id="roland-garros">
                     <Link className="text-white text-center h5" to="/race-to-london">Race To London</Link>
                 </div>
             </div>
             <div className="row d-flex justify-content-around">
-                <div className="mt-3 mb-4 col-sm-10 col-md-5 col-lg-4 d-flex justify-content-center align-items-center" id="wimbledon">
+                <div className="mt-3 mb-4 col-sm-10 col-md-5 col-lg-4 d-flex justify-content-center align-items-center miniature" id="wimbledon">
                     <Link className="text-white text-center h5" to="/current-tournaments" >Current Tournaments</Link>
                 </div>
-                <div className="mt-3 mb-4 col-sm-10 col-md-5 col-lg-4 d-flex justify-content-center align-items-center" id="us-open">
+                <div className="mt-3 mb-4 col-sm-10 col-md-5 col-lg-4 d-flex justify-content-center align-items-center miniature" id="us-open">
                     <Link className="text-white text-center h5" to="/season">Season Calendar</Link>
+                </div>
+            </div>
+            <div className="row d-flex justify-content-around">
+                <div className="mt-3 mb-4 col-sm-10 col-md-5 col-lg-4 d-flex justify-content-center align-items-center miniature" id="davis">
+                    <Link className="text-white text-center h5" to="/news" >News</Link>
+                </div>
+                <div className="mt-3 mb-4 col-sm-10 col-md-5 col-lg-4 d-flex justify-content-center align-items-center miniature" id="gold-medal">
+                    <Link className="text-dark text-center h5" to="/user-articles">Your saved articles</Link>
                 </div>
             </div>
         </div>
