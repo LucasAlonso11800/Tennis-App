7d89691d0d260041bae125f9648fd0b1b3d62599
Lucas <lucasalonso11800@gmail.com>
2021-05-17 22:23:34 -0300

    Fixed table bug

--- frontend/src/components/RankingTable/RankingTable.elements.js
+++ frontend/src/components/RankingTable/RankingTable.elements.js
@@ -1,9 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Colors } from '../../globalStyles';
 
 export const Table = styled.table`
 width: ${({ width }) => width ? `${width}px` : '100%'};
-@media all and (max-width: 535px){
+@media all and (max-width: 570px){
     display: block;
 }
 text-align: center;
@@ -12,7 +12,7 @@ text-align: center;
 export const TableHead = styled.thead`
 background-color: ${Colors.beige};
 color: ${Colors.darkBlue};
-@media all and (max-width: 535px){
+@media all and (max-width: 570px){
     position: absolute;
     top: -9999px;
     left: -9999px;
@@ -21,17 +21,17 @@ color: ${Colors.darkBlue};
 `;
 
 export const TableRow = styled.tr`
-    @media all and (max-width: 535px){
+    @media all and (max-width: 570px){
     display: block;
 }
 `;
 export const TableHeader = styled.th`
-    @media all and (max-width: 535px){
+    @media all and (max-width: 570px){
     display: block;
 }
 `;
 export const TableBody = styled.tbody`
-    @media all and (max-width: 535px){
+    @media all and (max-width: 570px){
     display: block;
 }
 `;
@@ -40,7 +40,7 @@ export const Player = styled.tr`
 background-color: ${Colors.beigeTransparent};
 color: ${Colors.darkBlue};
 font-weight: bold;
-@media all and (max-width: 535px){
+@media all and (max-width: 570px){
     display: block;
 }
 &:hover{
@@ -48,21 +48,21 @@ font-weight: bold;
 }
 `;
 
+const contentArray = ['Ranking', 'Player', 'Country', 'Points', 'Movement'];
 export const PlayerData = styled.td`
-@media all and (max-width: 535px){
+@media all and (max-width: 570px){
     position: relative;
     padding-left: 50%;
     padding-top: 0.5em;
     display: block;
-    border-top: ${({borderTop}) => borderTop ? `1px solid ${Colors.darkBlue}` : ''};
-    border-bottom: ${({borderBottom}) => borderBottom ? `1px solid ${Colors.darkBlue}` : ''};
-    ::before{
+    border-top: ${({ borderTop }) => borderTop ? `1px solid ${Colors.darkBlue}` : ''};
+    border-bottom: ${({ borderBottom }) => borderBottom ? `1px solid ${Colors.darkBlue}` : ''};
+::before{
         position: absolute;
         left: 6px;
         width: 45%;
         padding-right: 10px;
-        white-space: nowrap;
-        content: ${({content}) => content ? content : ''};
+        ${({ content }) => content >= 0 && css`content: '${contentArray[content]}';`}
     };
 }
-`
+`;
